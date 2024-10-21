1a62a9ca24c5eda2d875154bf776b9b4b83b23bb
ahsan-abdul <ahsan5308abdul@gmail.com>
2024-10-21 13:42:02 +0500

    Add files via upload

---
+++ my project 2 task 2.cpp
@@ -0,0 +1,107 @@
+//Ahsan Abdulrehman
+// section 2
+// Task 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "**********\n";
+	cout << "*\t*\n";
+	cout << "**********\n";
+
+
+
+	//task 2
+
+
+	cout << "************\n";
+	cout << "HELLO WORLD\n";
+	cout << "***********\n";
+
+
+	//task 3
+
+
+	cout << "  *\n";
+	cout << " * *\n";
+	cout << "*   *\n";
+	cout << "******\n";
+
+	//task 4
+
+
+	cout << "  * \n";
+	cout << " *** \n";
+	cout << "*****\n";
+
+	//task 5
+
+	cout << " **** \n";
+	cout << "*\t * \n";
+	cout << "*\t * \n";
+	cout << " **** \n";
+	
+
+	// task 6
+
+	cout << "*********\n";
+	cout << "*\t*\n";
+	cout << "*\t*\n";
+	cout << "*********\n";
+
+	//task 7
+	cout << "   *\n";
+	cout << " *   *\n";
+	cout << "*     *\n";
+	cout << " *   *\n";
+	cout << "   *\n";
+	  
+	//task 8 
+
+	cout << "    *\n";
+	cout << "  *****\n";
+	cout << "*********\n";
+	cout << "  *****\n";
+	cout << "    *\n";
+
+	//Task 9
+
+	cout << "@@@@@@@@@@@@@@" << endl;
+	cout << "@  @\t  @  @" << endl;
+	cout << "@ @\t   @ @" << endl;
+	cout << "@@\t    @@" << endl;
+	cout << "@\t     @" << endl;
+	cout << "@@@@@@@@@@@@@@" << endl;
+
+	//Task 10
+
+	cout << "*****************" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "*****************" << endl;
+	cout << "*****************" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "\t*" << endl;
+	cout << "*****************" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*****************" << endl;
+	return 0;
+}
