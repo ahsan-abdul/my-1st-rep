25241008a6ef6f2cfce7947cd130f9f3e5effd65
ahsan-abdul <ahsan5308abdul@gmail.com>
2024-10-28 12:46:22 +0500

    Add files via upload

---
+++ source.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// TASK 1
+
+	cout << " ** ** " << endl;
+	cout << " ***** " << endl;
+	cout << " ***** " << endl;
+	cout << "  ***  " << endl;
+	cout << "   *   " << endl;
+	
+	// TASK 2
+	// A
+	float result = 25 / 3;
+	cout << result << endl;
+
+	// B
+	
+	int sum = 20 - 12 / 4 * 2;
+	cout << sum << endl ;
+
+	//  C
+
+	int answer = 32 % 7;
+	cout << answer << endl;
+
+	//  D
+
+	int cal = 33 - 5 % 7;
+	cout << cal << endl;
+
+	// E 
+
+	float summ = 18.0 / 4;
+	cout << summ << endl;
+
+	//  F 
+
+	float ans = 18 - 5 / 2.0;
+	cout << ans << endl;
+
+	// G 
+
+	float mynum = 17 + 5 % 2 - 3;
+	cout << mynum << endl;
+
+	// H 
+
+	float mysum = 15.0 + 3.0 * 2.0 / 5.0;
+	cout << mysum << endl;
+
+	// TASK 3
+
+	cout << "************" << endl;
+	cout << "RESULT SHEET" << endl;
+	cout << "************" << endl;
+
+	cout << "STUDENTS ROLL NO.\t\tMATH\tPHYSICS\t CHEMISTRY\tTOTAL" << endl;
+	cout << "----------------------------------------------------------------------" << endl;
+	cout << "   2357 \t\t\t 99 \t 85 \t  97  \t\t 281" << endl;
+	cout << "----------------------------------------------------------------------" << endl;
+
+	//TASK 4
+
+	cout << "2 \tx \t1\t= \t2 " << endl;
+	cout << "2 \tx  \t2\t= \t4 " << endl;
+	cout << "2 \tx \t3\t= \t6 " << endl;
+	cout << "2 \tx \t4\t= \t8 " << endl;
+	cout << "2 \tx \t5\t= \t10 " << endl;
+	cout << "2 \tx \t6\t= \t12 " << endl;
+	cout << "2 \tx \t7\t= \t14 " << endl;
+	cout << "2 \tx \t8\t= \t16 " << endl;
+	cout << "2 \tx \t9\t= \t18 " << endl;
+	cout << "2 \tx \t10\t= \t20 " << endl;
+	
+	//TASK 5
+
+	cout << " CCCCCCCCC " << endl;
+	cout << " CC\n CC\n CC\n CC " << endl;
+	cout << " CCCCCCCCC " << endl;
+    
+	//TASK 6 
+
+	cout << "\t**" << endl;
+	cout << "\t**" << endl;
+	cout << "****************" << endl;
+	cout << "****************" << endl;
+	cout << "\t**" << endl;
+	cout << "\t**" << endl;
+
+	// task 7 
+
+	cout << "*****************************************" << endl;
+	cout << "* \tPROGRAMING ASSIGNMENT 1\t\t*" << endl;
+	cout << "* \t COMPUTER PROGRAMMING \t \t*" << endl;
+	cout << "* \t\tAUTHOR:???\t\t* " << endl;
+	cout << "*\tDUE DATE:THURSDAY , JAN. 24\t*" << endl;
+	cout << "*****************************************" << endl;
+
+    // TASK 8 
+
+	cout << "  ***  " << endl;
+	cout << " ***** " << endl;
+	cout << "   *   " << endl;
+	cout << "   *   " << endl;
+
+	// TASK 9
+
+	cout << "*****" << endl;
+	cout << "*   *" << endl;
+	cout << "*****" << endl;
+
+	// TASK 10
+
+	cout << "  *  " << endl;
+	cout << "*****" << endl;
+	cout << "  *  " << endl;
+    return 0;
+
+}
