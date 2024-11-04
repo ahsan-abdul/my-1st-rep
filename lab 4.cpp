6113de440624c08c0a5d0ba48ebe5e61f67ca2b1
ahsan-abdul <ahsan5308abdul@gmail.com>
2024-11-04 13:41:54 +0500

    Add files via upload

---
+++ lab 4.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//// task 1
+	//float n=0;
+	//cout << "press any key to continue" << endl;
+	//cin >> n;
+	//cout <<"variable value = " << n << endl;
+
+
+	//// task 2
+
+	//float no = 0;
+	//cout << "press any key to continue" << endl;
+	//cin >> no;
+	//cout << " enter variable value  " << no << endl;
+	//cout << "variable value entered = " << no << endl;
+
+	////// task 3
+
+	//float No1 = 0;
+	//float No2 = 0;
+	//cout << " enter value of first  variable = " << endl;
+ //   cin >> No1;
+	//cout << " enter value of second variable = " << endl;
+	//cin >> No2;
+	//cout << "sum of values = " << (No1 + No2) << endl;
+	//cout << "difference of values = " << (No2 - No1) << endl; 
+	//cout << "product of values = " << (No1 * No2) << endl; 
+	//cout << "division of values = " << (No1 / No2) << endl;
+
+	//// task 4
+
+	//int No3 = 0;
+	//int No4 = 0;
+	//int No5 = 0;
+	//cout << "enter the amount  of fuel = " << endl;
+	//cin >> No3;
+	//cout << "enter the amount of rent = " << endl;
+	//cin >> No4;
+	//cout << "enter the amount of bills = " << endl;
+	//cin >> No5;
+	//cout << "----------------------------------------" << endl;
+	//
+	//cout << "fuel\t\t\t" << No3 << endl;
+ //   
+	//cout << "rent\t\t\t" << No4 << endl;
+
+	//cout << "bills\t\t\t" << No5 << endl;
+
+	//cout << "total\t\t\t" << (No3 + No4 + No5) << endl;
+	//cout << "----------------------------------------" << endl;
+
+
+
+	////task 5
+
+
+	//int NO1 = 0;
+	//int NO2 = 0;
+	//int NO3 = 0;
+	//cout << "enter value of chemistry : " << endl;
+	//cin >> NO1;
+	//cout << "enter value of physics : " << endl;
+	//cin >> NO2;
+	//cout << " enter value of mathematics : " << endl;
+	//cin >> NO3;
+	//cout << "-------------------------------------------------" << endl;
+	//cout << "maths\t\tchemistry\tphysics\ttotal" << endl;
+	//cout << NO3 << "\t\t" << NO1 << "\t" << NO2 << "\t" << (NO1 + NO2 + NO3) << endl;
+	//cout << "-------------------------------------------------" << endl;
+
+	//task 6
+
+
+	int nm1 = 0;
+	int nm2 = 0;
+	int nm3 = 0;
+	int nm4 = 0;
+	int nm5 = 0;
+	int nm6 = 0;
+	cout << "enter the price of wheat:" << endl;
+	cin >> nm1;
+	cout << "enter the quantity of wheat:" << endl;
+	cin >> nm2;
+	cout << "enter the price of rice:" << endl;
+	cin >> nm3;
+	cout << "enter the quantity of rice:" << endl;
+	cin >> nm4;
+	cout << "enter the price of suagr :" << endl;
+	cin >> nm5;
+	cout << "enter the quantity of sugar :" << endl;
+	cin >> nm6;
+	cout << "-------------------------------" << endl;
+	cout << "value of wheat : " << nm1 * nm2 << endl;
+	cout << "value of rice : " << nm3 * nm4 << endl;
+	cout << "value of sugar : " << nm5 * nm6 << endl;
+	cout << "-------------------------------" << endl;
+	return 0;
+}
