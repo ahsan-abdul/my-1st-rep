11bb55344b2234633bdfa3a99f9150b87a7fb19f
ahsan-abdul <ahsan5308abdul@gmail.com>
2024-11-11 13:36:34 +0500

    Add files via upload

---
+++ lab 5.cpp
@@ -0,0 +1,219 @@
+//Name:Ahsan abdulrehman
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//	// task 1
+	//
+	//	int no1 = 0, no2 = 0, no3 = 0;
+	//	cout << "enter the marks of first subject : " << endl;
+	//	cin >> no1;
+	//	cout << "enter the marks of second subject : " << endl;
+	//	cin >> no2;
+	//	cout << "enter the marks of the third subject : " << endl;
+	//	cin >> no3;
+	//	if (((no1 + no2 + no3) / 3) > 80)
+	//	{
+	//		cout << "you are above standard!" << endl;
+	//		cout << "admission granted" << endl;
+	//	}
+
+
+		// task 3
+
+		//int num1 = 0;
+		//int num2 = 0;
+		//int num3 = 0;
+		//cout << "enter the first number : " << endl;
+		//cin >> num1;
+		//cout << "enter the second number : " << endl;
+		//cin >> num2;
+		//cout << "enter the third number : " << endl;
+		//cin >> num3;
+		//if (num1 >num2&&num1>num3)
+		//{
+		//	cout << "the number is maximum"<<num1 << endl;
+		//}
+		//else
+		//{
+
+		//}
+		//if (num2 > num3 && num2 > num1)
+		//{
+		//	cout << "the number is maximum" << num2 << endl;
+		//}
+		//else
+		//{
+
+		//}
+		//if (num3 > num1 && num3 >> num2)
+		//{
+		//	cout << "the number is maximum" << num3 << endl;
+		//}
+		//else
+		//{
+
+		//}
+
+		//task 6
+
+		//int n1 = 0;
+		//int n2 = 0;
+		//cout << "enter the first integer = " << endl;
+		//cin >> n1;
+		//cout << "enter the second integer = " << endl;
+		//cin >> n2;
+		//if (n1 % n2==0 ) 
+		//{
+		//	cout << n1 << " is the multiple of " << n2 << endl;
+		//}
+		//else
+		//{
+		//	cout << n1 << " is not the multiple of " << n2 << endl;
+		//}
+
+		////task 5
+
+		//int nmb = 0;
+		//cout << "enter the score " << endl;
+		//cin >> nmb;
+		//if (nmb > 50)
+		//{
+		//	cout << "you passed " << endl;
+		//}
+		//else
+		//{
+		//	cout << "you failed " << endl;
+		//}
+
+	//	//task 7
+
+	//int nm1 = 0;
+	//int nm2 = 0;
+	//char op;
+	//cout << "enter two integers : ";
+	//cin >> nm1 >> nm2;
+	//cout << "enter an operation(+,-,*,/,%):";
+	//cin >> op;
+	//if (op == '+')
+	//{
+	//	cout << "sum: " << nm1 + nm2 << endl;
+	//}
+	//else
+	//{
+
+	//}
+	//if (op == '-')
+	//{
+	//	cout << "difference: " << nm1 - nm2 << endl;
+	//}
+	//else
+	//{
+
+	//}
+	//if (op == '*')
+	//{
+	//	cout << "multiply: " << nm1 * nm2 << endl;
+	//}
+	//else
+	//{
+
+	//}
+	//if (op == '/')
+	//{
+	//	cout << "divide: " << nm1 / nm2 << endl;
+	//}
+	//else
+	//{
+
+	//}
+	//if (op == '%')
+	//{
+	//	cout << "modulus: " << nm1 % nm2 << endl;
+	//}
+	//else
+	//{
+
+	//}
+	//if (op == '/' && nm2 >> 0)
+	//{
+	//	cout << "division: " << nm1 / nm2 << endl;
+	//}
+	//else
+	//{
+	//	cout << "invalid operation or divisionby zero " << endl;
+	//}
+
+	//task 8
+
+	float A = 0;
+	float B = 0;
+	float C = 0;
+	float D = 0;
+	float E = 0;
+	float F = 0;
+	cout << "A : " << endl;
+	cin >> A;
+	cout << "B : " << endl;
+	cin >> B;
+	cout << "C : " << endl;
+	cin >> C;
+	cout << "D : " << endl;
+	cin >> D;
+	cout << "E : " << endl;
+	cin >> E;
+	cout << "F: " << endl;
+	cin >> F;
+
+	if (A > B && A > C && A > D && A > E && A > F)
+	{
+		cout << "A got the heighest average" << endl;
+	}
+	else
+	{
+
+	}
+	if (B > A && B > C && B > D && B > E && B > F)
+	{
+		cout << "B got the heighest average" << endl;
+	}
+	else
+	{
+
+	}
+	if (C > B && C > A && C > D && C > E && C > F)
+	{
+		cout << "C got the hieghest average" << endl;
+	}
+	else
+	{
+
+	}
+	if (D > A && D > B && D > C && D > E && D > F)
+	{
+		cout << "D got the heighest average" << endl;
+	}
+	else
+	{
+
+	}
+	if (E > A && E > B && E > C && E > D && E > F)
+	{
+		cout << "E got the heighest average" << endl;
+	}
+	else
+	{
+
+	}
+	if (F > B && F > C && F > D && F > E && F > A)
+	{
+		cout << "F got the heighest average" << endl;
+	}
+	else
+	{
+
+	}
+		return 0;
+}
